ec9977ef119721ff16a05499191f919a9820c395
trunghai04 <163894721+trunghai04@users.noreply.github.com>
2024-03-30 14:41:56 +0700

    Update CPP0117 - TÍNH TỔNG CHỮ SỐ.cpp

--- CPP0117 - TÍNH TỔNG CHỮ SỐ.cpp
+++ CPP0117 - TÍNH TỔNG CHỮ SỐ.cpp
@@ -1,20 +1,9 @@
-// Created by Nguyễn Mạnh Quân
 
-#include<bits/stdc++.h>
+#include<iostream>
+#include <algorithm>
 
 using namespace std;
 
-#define mp make_pair
-#define fi first
-#define se second
-#define pb push_back
-#define sz size()
-#define ll long long
-#define FOR(i, a, b) for(int i = a; i <= b; ++i)
-#define FORD(i, a, b) for(int i = a; i >= b; --i)
-#define faster() ios_base::sync_with_stdio(0); cin.tie(NULL);cout.tie(NULL);
-#define vi vector<int>
-#define vll vector<ll>
 
 int calc(int n)
 {
@@ -29,7 +18,6 @@ int calc(int n)
 
 int main()
 {
-    faster();
     int t;
     cin >> t;
     while(t--)
