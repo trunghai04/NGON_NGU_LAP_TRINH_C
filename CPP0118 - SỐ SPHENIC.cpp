b69b3c510f399a5c6fc81d0db86f749122fca42f
trunghai04 <163894721+trunghai04@users.noreply.github.com>
2024-03-30 14:38:48 +0700

    Update CPP0118 - SỐ SPHENIC.cpp

--- CPP0118 - SỐ SPHENIC.cpp
+++ CPP0118 - SỐ SPHENIC.cpp
@@ -1,20 +1,8 @@
-// Created by Nguyễn Mạnh Quân
-
-#include<bits/stdc++.h>
+#include<iostream>
+#include <algorithm>
 
 using namespace std;
 
-#define mp make_pair
-#define fi first
-#define se second
-#define pb push_back
-#define sz size()
-#define ll long long
-#define FOR(i, a, b) for(int i = a; i <= b; ++i)
-#define FORD(i, a, b) for(int i = a; i >= b; --i)
-#define faster() ios_base::sync_with_stdio(0); cin.tie(NULL);cout.tie(NULL);
-#define vi vector<int>
-#define vll vector<ll>
 
 bool check(int n)
 {
@@ -36,7 +24,6 @@ bool check(int n)
 
 int main()
 {
-    faster();
     int t;
     cin >> t;
     while(t--)
