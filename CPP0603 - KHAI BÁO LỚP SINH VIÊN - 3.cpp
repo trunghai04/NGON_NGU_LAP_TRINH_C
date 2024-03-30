cb5d67b45b9cc1e3e4141598f38ffe5dbfb6763e
trunghai04 <163894721+trunghai04@users.noreply.github.com>
2024-03-30 22:43:56 +0700

    Update CPP0603 - KHAI BÁO LỚP SINH VIÊN - 3.cpp

--- CPP0603 - KHAI BÁO LỚP SINH VIÊN - 3.cpp
+++ CPP0603 - KHAI BÁO LỚP SINH VIÊN - 3.cpp
@@ -1,25 +1,6 @@
-// Created by Nguyễn Mạnh Quân
-
 #include<bits/stdc++.h>
 
 using namespace std;
-
-#define mp make_pair
-#define fi first
-#define se second
-#define pb push_back
-#define sz size()
-#define ll long long
-#define FOR(i, a, b) for(int i = a; i <= b; ++i)
-#define FORD(i, a, b) for(int i = a; i >= b; --i)
-#define F(i, a, b) for(int i = a; i < b; ++i)
-#define FD(i, a, b) for(int i = a; i > b; --i)
-#define faster() ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
-#define vi vector<int>
-#define vll vector<ll>
-#define vb vector<bool>
-#define PI 3.141592653589793238
-#define endl '\n'
 #define a() a
 
 class SinhVien
@@ -44,7 +25,7 @@ class SinhVien
         while(ss >> s)
         {
             s[0] = toupper(s[0]);
-            F(i, 1, s.length()) s[i] = tolower(s[i]);
+            for(int i=1;i<s.length();i++) s[i] = tolower(s[i]);
             a.ten += s + " ";
         }
         a.ten.erase(a.ten.end() - 1);
