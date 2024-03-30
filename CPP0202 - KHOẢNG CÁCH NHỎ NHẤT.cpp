f0e8cbbed3bc3de93cdd3f275b6ef230404f62a2
trunghai04 <163894721+trunghai04@users.noreply.github.com>
2024-03-30 14:18:00 +0700

    Update CPP0202 - KHOẢNG CÁCH NHỎ NHẤT.cpp

--- CPP0202 - KHOẢNG CÁCH NHỎ NHẤT.cpp
+++ CPP0202 - KHOẢNG CÁCH NHỎ NHẤT.cpp
@@ -1,36 +1,23 @@
-// Created by Nguyễn Mạnh Quân
 
-#include<bits/stdc++.h>
+#include<iostream>
+#include <algorithm>
 
 using namespace std;
 
-#define mp make_pair
-#define fi first
-#define se second
-#define pb push_back
-#define sz size()
-#define ll long long
-#define FOR(i, a, b) for(int i = a; i <= b; ++i)
-#define FORD(i, a, b) for(int i = a; i >= b; --i)
-#define faster() ios_base::sync_with_stdio(0); cin.tie(NULL);cout.tie(NULL);
-#define vi vector<int>
-#define vll vector<ll>
-#define endl '\n'
 
 int a[100005];
 
 int main()
 {
-    faster();
     int t, n;
     cin >> t;
     while(t--)
     {
         int ans = INT_MAX;
         cin >> n;
-        FOR(i, 1, n) cin >> a[i];
+        for(int i=1; i<=n; i++) cin >> a[i];
         sort(a + 1, a + 1 + n);
-        FOR(i, 2, n) ans = min(ans, a[i] - a[i - 1]);
+        for(int i=2; i<=n;i++) ans = min(ans, a[i] - a[i - 1]);
         cout << ans << endl;
     }
     return 0;
