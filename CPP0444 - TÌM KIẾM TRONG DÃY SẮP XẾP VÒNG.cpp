eaf70d8d7b069229796447baf5363dbf68df2c0a
trunghai04 <163894721+trunghai04@users.noreply.github.com>
2024-03-30 23:06:17 +0700

    Update CPP0444 - TÌM KIẾM TRONG DÃY SẮP XẾP VÒNG.cpp

--- CPP0444 - TÌM KIẾM TRONG DÃY SẮP XẾP VÒNG.cpp
+++ CPP0444 - TÌM KIẾM TRONG DÃY SẮP XẾP VÒNG.cpp
@@ -1,25 +1,8 @@
-// Created by Nguyễn Mạnh Quân
-
 #include<bits/stdc++.h>
 
 using namespace std;
 
-#define mp make_pair
-#define fi first
-#define se second
-#define pb push_back
-#define sz size()
-#define ll long long
-#define FOR(i, a, b) for(int i = a; i <= b; ++i)
-#define FORD(i, a, b) for(int i = a; i >= b; --i)
-#define F(i, a, b) for(int i = a; i < b; ++i)
-#define FD(i, a, b) for(int i = a; i > b; --i)
-#define faster() ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
-#define vi vector<int>
-#define vll vector<ll>
-#define vb vector<bool>
-#define PI 3.141592653589793238
-#define endl '\n'
+
 
 int binSearchMin(int a[], int l, int r, int n)
 {
@@ -48,7 +31,6 @@ int binSearch(int a[], int l, int r, int x)
 
 int main()
 {
-    faster();
     int t, n, x, posMin;
     cin >> t;
     while(t--)
