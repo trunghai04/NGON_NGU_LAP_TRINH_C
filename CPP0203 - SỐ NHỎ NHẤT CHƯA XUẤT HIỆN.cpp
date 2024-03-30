3d2ac485171c5100c4728b056b6533bd999e7d53
trunghai04 <163894721+trunghai04@users.noreply.github.com>
2024-03-30 14:23:22 +0700

    Update CPP0203 - SỐ NHỎ NHẤT CHƯA XUẤT HIỆN.cpp

--- CPP0203 - SỐ NHỎ NHẤT CHƯA XUẤT HIỆN.cpp
+++ CPP0203 - SỐ NHỎ NHẤT CHƯA XUẤT HIỆN.cpp
@@ -1,39 +1,25 @@
-// Created by Nguyễn Mạnh Quân
-
-#include<bits/stdc++.h>
+#include<iostream>
+#include <algorithm>
 
 using namespace std;
 
-#define mp make_pair
-#define fi first
-#define se second
-#define pb push_back
-#define sz size()
-#define ll long long
-#define FOR(i, a, b) for(int i = a; i <= b; ++i)
-#define FORD(i, a, b) for(int i = a; i >= b; --i)
-#define faster() ios_base::sync_with_stdio(0); cin.tie(NULL);cout.tie(NULL);
-#define vi vector<int>
-#define vll vector<ll>
-#define endl '\n'
 
 int a[100005];
 
 int main()
 {
-    faster();
     int t, n;
     cin >> t;
     while(t--)
     {
         cin >> n;
         int dd[1000005] = {};
-        FOR(i, 1, n)
+        for(int i=1; i<=n;i++)
         {
             cin >> a[i];
             if(a[i] > 0) dd[a[i]]++;
         }
-        FOR(i, 1, 1000000)
+        for(int i=1; i<=1000000;i++)
         {
             if(!dd[i])
             {
