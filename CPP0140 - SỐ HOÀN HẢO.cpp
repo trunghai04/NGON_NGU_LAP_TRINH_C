b2be7b52409e8d81d21e22e5991c358c8e7784e2
trunghai04 <163894721+trunghai04@users.noreply.github.com>
2024-03-30 09:30:43 +0700

    Update CPP0140 - SỐ HOÀN HẢO.cpp

--- CPP0140 - SỐ HOÀN HẢO.cpp
+++ CPP0140 - SỐ HOÀN HẢO.cpp
@@ -1,20 +1,9 @@
-// Created by Nguyễn Mạnh Quân
-
-#include<bits/stdc++.h>
+#include<iostream>
+#include <cmath>
 
 using namespace std;
 
-#define mp make_pair
-#define fi first
-#define se second
-#define pb push_back
-#define sz size()
 #define ll long long
-#define FOR(i, a, b) for(int i = a; i <= b; i++)
-#define FORD(i, a, b) for(int i = a; i >= b; i--)
-#define faster() ios_base::sync_with_stdio(0); cin.tie(NULL);cout.tie(NULL);
-#define vi vector<int>
-#define vll vector<ll>
 
 bool Solve()
 {
@@ -38,7 +27,6 @@ bool Solve()
 
 int main()
 {
-    faster();
     int t;
     cin >> t;
     while(t--) cout << Solve() << endl;
