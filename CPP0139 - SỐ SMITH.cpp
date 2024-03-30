7325536ef53220818af750d10ff69ea0c7437dce
trunghai04 <163894721+trunghai04@users.noreply.github.com>
2024-03-30 09:36:39 +0700

    Update CPP0139 - SỐ SMITH.cpp

--- CPP0139 - SỐ SMITH.cpp
+++ CPP0139 - SỐ SMITH.cpp
@@ -1,6 +1,5 @@
-// Created by Nguyễn Mạnh Quân
-
-#include<bits/stdc++.h>
+#include<iostream>
+#include <math.h>
 
 using namespace std;
 
@@ -10,9 +9,6 @@ using namespace std;
 #define pb push_back
 #define sz size()
 #define ll long long
-#define FOR(i, a, b) for(int i = a; i <= b; i++)
-#define FORD(i, a, b) for(int i = a; i >= b; i--)
-#define faster() ios_base::sync_with_stdio(0); cin.tie(NULL);cout.tie(NULL);
 #define vi vector<int>
 #define vll vector<ll>
 
@@ -29,11 +25,17 @@ int sumNum(int n)
 
 int main()
 {
-    faster();
     int N = 100005;
     bool check[100007];
-    FOR(i, 2, N) check[i] = 1;
-    FOR(i, 2, N) if(check[i]) for(int j = 2 * i; j <= N; j += i) check[j] = 0;
+    for(int i=2; i<=N; i++) check[i] = 1;
+    for(int i=2; i<=N; i++) 
+    {
+        if(check[i]) 
+        {
+            for(int j = 2 * i; j <= N; j += i) 
+            check[j] = 0;
+        }
+    }
     int t;
     cin >> t;
     while(t--)
