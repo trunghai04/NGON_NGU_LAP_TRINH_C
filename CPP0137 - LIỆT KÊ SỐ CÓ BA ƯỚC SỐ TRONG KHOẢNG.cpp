78eadbaf5b8e05700f593eeafc80d2c3c8ced670
trunghai04 <163894721+trunghai04@users.noreply.github.com>
2024-03-30 09:37:26 +0700

    Update CPP0137 - LIỆT KÊ SỐ CÓ BA ƯỚC SỐ TRONG KHOẢNG.cpp

--- CPP0137 - LIỆT KÊ SỐ CÓ BA ƯỚC SỐ TRONG KHOẢNG.cpp
+++ CPP0137 - LIỆT KÊ SỐ CÓ BA ƯỚC SỐ TRONG KHOẢNG.cpp
@@ -1,4 +1,3 @@
-// Created by Nguyễn Mạnh Quân
 
 #include<bits/stdc++.h>
 
@@ -12,7 +11,6 @@ using namespace std;
 #define ll long long
 #define FOR(i, a, b) for(int i = a; i <= b; i++)
 #define FORD(i, a, b) for(int i = a; i >= b; i--)
-#define faster() ios_base::sync_with_stdio(0); cin.tie(NULL);cout.tie(NULL);
 #define vi vector<int>
 #define vll vector<ll>
 
