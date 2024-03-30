12c7a193f5d74778f04df15ca60c6300de2e0bd5
trunghai04 <163894721+trunghai04@users.noreply.github.com>
2024-03-30 22:27:58 +0700

    Update CPP0202 - KHOẢNG CÁCH NHỎ NHẤT.cpp

--- CPP0202 - KHOẢNG CÁCH NHỎ NHẤT.cpp
+++ CPP0202 - KHOẢNG CÁCH NHỎ NHẤT.cpp
@@ -1,23 +1,32 @@
-
 #include<iostream>
-#include <algorithm>
+#include <climits>
 
 using namespace std;
 
+void sort(int arr[], int n) {
+    for (int i = 1; i < n; ++i) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
 
 int a[100005];
 
-int main()
-{
+int main() {
     int t, n;
     cin >> t;
-    while(t--)
-    {
+    while (t--) {
         int ans = INT_MAX;
         cin >> n;
-        for(int i=1; i<=n; i++) cin >> a[i];
-        sort(a + 1, a + 1 + n);
-        for(int i=2; i<=n;i++) ans = min(ans, a[i] - a[i - 1]);
+        for (int i = 1; i <= n; i++) cin >> a[i];
+        sort(a + 1, n);
+        for (int i = 2; i <= n; i++) 
+			ans = min(ans, a[i] - a[i - 1]);
         cout << ans << endl;
     }
     return 0;
